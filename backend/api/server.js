0e9c9dc1e55e1309dcb47e7c41ae8b520fe45104
Anurag Tiwari <150263465+sanataniLadkaa@users.noreply.github.com>
2025-01-26 00:27:00 +0530

    Update server.js

--- backend/api/server.js
+++ backend/api/server.js
@@ -15,7 +15,7 @@ app.use(cors({
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/rental-management', {
+mongoose.connect('mongodb://<AnuragTiwari>:<Anurag>@<cluster>.mongodb.net/rental-management', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
