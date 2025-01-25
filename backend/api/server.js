30974ba792defdd171f27b9be62566590b1e1f79
Anurag Tiwari <150263465+sanataniLadkaa@users.noreply.github.com>
2025-01-26 00:40:26 +0530

    Update server.js

--- backend/api/server.js
+++ backend/api/server.js
@@ -15,7 +15,7 @@ app.use(cors({
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://AnuragTiwari:<db_password>@<hostname>/?ssl=true&replicaSet=atlas-sb485l-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect('mongodb://AnuragTiwari:Anurag/?ssl=true&replicaSet=atlas-sb485l-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
